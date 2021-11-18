b82e37df44fba18ce25eb02a9d96993f535a692c
Antonin06 <avon.antonin@gmai.com>
2021-11-18 22:22:02 +0100

    ReactLikeAMerguez - V1

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./assets/scss/style.scss";
+import 'bootstrap';
+
+function App() {
+  return (
+    <div className="app">
+        <div className="intro">BoilerPlate React</div>
+    </div>
+  );
+}
+
+export default App;
